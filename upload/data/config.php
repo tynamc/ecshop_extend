132ff6c07d7a537ee2cc431643b6c2677baf071c
tynamc <soloheron001@gmail.com>
2014-08-18 00:11:52 +0800

    升级补丁安装并取出demo install

--- upload/data/config.php
+++ upload/data/config.php
@@ -30,6 +30,6 @@ define('AUTH_KEY', 'this is a key');
 
 define('OLD_AUTH_KEY', '');
 
-define('API_TIME', '');
+define('API_TIME', '2014-08-18 00:01:14');
 
 ?>
--- upload/temp/query_caches/sqlcache_config_file_6aa327e593a5d0a9b160f5a13a94a4b8.php
+++ upload/temp/query_caches/sqlcache_config_file_6aa327e593a5d0a9b160f5a13a94a4b8.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1408207791;
+$this->mysql_config_cache_file_time = 1408291757;
 $this->timeline = 0;
 $this->timezone = 0;
 $this->platform = 'WINDOWS';
